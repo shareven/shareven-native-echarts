6a922fbd974df618d77a1e294d073b2d825f7c26
shareven <1060776877@qq.com>
2020-02-09 23:55:43 +0800

    fix err

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,12 @@
-declare class Echarts{
-
-};
+declare module 'shareven-native-echarts' {
+    import * as React from 'react';
+    import * as ReactNative from 'react-native';
+  
+  
+    export class Echarts extends React.Component {}
+  
+    export default Echarts;
+  }
 
 export { Echarts };
 export default Echarts;
