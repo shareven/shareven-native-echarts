11b37b08b9ae4a8961fc9154f506c8852995a239
shareven <1060776877@qq.com>
2020-02-10 00:14:52 +0800

    add declare

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,31 @@
-declare module 'shareven-native-echarts' {
+declare module 'shareven-native-echarts' 
+{
     import * as React from 'react';
     import * as ReactNative from 'react-native';
   
-  
-    export class Echarts extends React.Component {}
+    export interface EchartsProps extends ReactNative.ViewProps {
+      option: {
+        title: {
+          text: string;
+        };
+        tooltip: {};
+        legend: {
+          data: string[];
+        };
+        xAxis: {
+          data: string[];
+        };
+        yAxis: {};
+        series: {
+          name: string;
+          type: string;
+          data: number[];
+        }[];
+      },
+      height?:number
+    }
+
+    export class Echarts extends React.Component<EchartsProps> {}
   
     export default Echarts;
   }
-
-export { Echarts };
-export default Echarts;
